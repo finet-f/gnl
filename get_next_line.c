7c7102f1668b9e0ec0aea4ab894182d0d6c62de5
Florian Finet <flfinet@z4r2p4.le-101.fr>
2018-10-15 16:23:40 +0200

    test

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: flfinet <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/11 10:57:18 by flfinet      #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/11 17:01:15 by flfinet     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/15 16:21:52 by flfinet     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -16,128 +16,131 @@
 #include <stdio.h>
 #include "get_next_line.h"
 
-char	*ft_realloc(char *str)
+char			*ft_realloc(char *str)
 {
-  char	*new;
-  int	l;
-  int	i;
+	char		*new;
+	int			l;
+	int			i;
 
-  i = 0;
-  l = 0;
-  if (str == NULL || str[0] == 0)
-    l = 1;
-  else
-    while (str[l])
-      l++;
-  new = malloc(sizeof(char) * (l * BUFF_SIZE + 1));
-  if (new == NULL)
-    return (NULL);
-  if (str != NULL)
-    {
-      while (str[i])
+	i = 0;
+	l = 0;
+	if (str == NULL || str[0] == 0)
+		l = 1;
+	else
+		while (str[l])
+			l++;
+	new = malloc(sizeof(char) * (l * BUFF_SIZE + 1));
+	if (new == NULL)
+		return (NULL);
+	if (str != NULL)
 	{
-	  new[i] = str[i];
-	  i++;
+		while (str[i])
+		{
+			new[i] = str[i];
+			i++;
+		}
 	}
-    }
-  new[i] = '\0';
-  return (new);
+	new[i] = '\0';
+	return (new);
 }
 
-char	*modif_rest(char *rest, int i)
+char			*modif_rest(char *rest, int i)
 {
-  char	*new;
-  int	j;
+	char		*new;
+	int			j;
 
-  j = 0;
-  new = NULL;
-  new = ft_realloc(new);
-  if (rest[i] == '\n')
-     i++;
-  while (rest[i])
-    {
-      new[j] = rest[i];
-      i++;
-      j++;
-    }
-  new[j] = '\0';
-  return (new);
-}
-
-int	get_next_lin3(char **list, int *i, int j,  char buff[BUFF_SIZE + 1], char *rest)
-{
-  int	k;
-
-  k = 0;
-  while (buff[k] != '\0' && buff[k] != '\n')
-    {
-      list[j][*i] = buff[k];
-      *i = *i + 1;
-      k++;
-    }
-  list[j][*i] = '\0';
-  if (buff[k] == '\n')
-    {
-      *i = 0;
-      k++;
-      while (buff[k])
+	j = 0;
+	new = NULL;
+	new = ft_realloc(new);
+	if (rest[i] == '\n')
+		i++;
+	while (rest[i])
 	{
-	  rest[*i] = buff[k];
-	  *i = *i + 1;
-	  k++;
+		new[j] = rest[i];
+		i++;
+		j++;
 	}
-      rest[*i] = '\0';
-      return (1);
-    }
-  return (0);
+	new[j] = '\0';
+	return (new);
 }
 
-char	*get_next_line2(char **list, char *rest, int i, int j, int fd)
+int				get_next_lin3(char **line, int *i, char buff[BUFF_SIZE + 1],
+		char *rest)
 {
-  int	r;
-  char	buff[BUFF_SIZE + 1];
+	int			k;
 
-  while (1)
-    {
-      r = read(fd, buff, BUFF_SIZE);
-      if (r == -1 || (r == 0 && list[j] == '\0'))
-	return (NULL);
-      if (r == 0)
-	return (list[j]);
-      buff[r] = '\0';
-      list[j] = ft_realloc(list[j]);
-      if (get_next_lin3(&list[j], &i, j,  buff, rest) == 1)
-	return (list[j]);
-    }
-  return (list[j]);
+	k = 0;
+	while (buff[k] != '\0' && buff[k] != '\n')
+	{
+		line[0][*i] = buff[k];
+		*i = *i + 1;
+		k++;
+	}
+	line[0][*i] = '\0';
+	if (buff[k] == '\n')
+	{
+		*i = 0;
+		k++;
+		while (buff[k])
+		{
+			rest[*i] = buff[k];
+			*i = *i + 1;
+			k++;
+		}
+		rest[*i] = '\0';
+		return (1);
+	}
+	return (0);
 }
 
-int		get_next_line(const int fd, char **list)
+char			*get_next_line2(char **line, char *rest, int i, int fd)
 {
-  static char	*rest;
-  static int j;
-  int		i;
-  char		c;
+	int			r;
+	char		buff[BUFF_SIZE + 1];
 
-  if (rest == NULL)
-    {
-      rest = ft_realloc(rest);
-      rest[0] = 0;
-          }
-  i = 0;
-  list[j] = ft_realloc(list[j]);
-  while (rest[i] != '\n' && rest[i] != '\0')
-    {
-      list[j][i] = rest[i];
-      i++;
-    }
-  list[j][i] = '\0';
-  c = rest[i];
-  rest = modif_rest(rest, i);
-  if (c == '\n')
-    return (0);
-  list[j] = ft_realloc(list[j]); 
-  get_next_line2(&list[j], rest, i, j, fd);
-  printf("%s\n", list[j]);
-  return (get_next_line(fd, &list[j++]));
- }
+	while (1)
+	{
+		r = read(fd, buff, BUFF_SIZE);
+		if (r == -1 || (r == 0 && line[0] == '\0'))
+			return (NULL);
+		if (r == 0)
+			return (line[0]);
+		buff[r] = '\0';
+		line[0] = ft_realloc(line[0]);
+		if (get_next_lin3(&line[0], &i, buff, rest) == 1)
+			return (line[0]);
+	}
+	return (line[0]);
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	static char	*rest;
+	int			i;
+	char		c;
+
+	if (rest == NULL)
+	{
+		rest = ft_realloc(rest);
+		rest[0] = 0;
+	}
+	i = 0;
+	if ((line == malloc(sizeof(char*) * 150) == 0))
+ 	  return (-1);
+	line[0] = ft_realloc(line[0]);
+	while (rest[i] != '\n' && rest[i] != '\0')
+	{
+		line[0][i] = rest[i];
+		i++;
+	}
+	line[0][i] = '\0';
+	c = rest[i];
+	rest = modif_rest(rest, i);
+	if (c == '\n')
+		return (0);
+	printf("%s\n", line[0]);
+	line[0] = ft_realloc(line[0]);
+	printf("%s\n", line[0]);
+	get_next_line2(&line[0], rest, i, fd);
+	return (get_next_line(fd, line));
+}
--- get_next_line.h
+++ get_next_line.h
@@ -1,10 +1,23 @@
-#ifndef GET_NEXT_LINE_H_
-# define GET_NEXT_LINE_H_
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   get_next_line.h                                  .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: flfinet <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/10/15 14:41:19 by flfinet      #+#   ##    ##    #+#       */
+/*   Updated: 2018/10/15 16:23:10 by flfinet     ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # ifndef BUFF_SIZE
-#  define BUFF_SIZE (199)
-# endif /* !BUFF_SIZE */
+#  define BUFF_SIZE (100)
+# endif
 
-int	get_next_line(const int fd, char **list);
+int	get_next_line(const int fd, char **line);
 
-#endif /* GET_NEXT_LINE_H_ */
+#endif
